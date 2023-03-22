5333397209ee17296aab465b18b6172eef3ab478
oarorvik <oarorvik@stud.ntnu.no>
2023-03-22 10:01:13 +0100

    Untested story_c. Needs a and b first to test.

--- story_c.py
+++ story_c.py
@@ -6,14 +6,27 @@ Denne funksjonaliteten skal programmeres.
 '''
 
 def main():
+    station = input("Name of station: ")
+    weekday = input("Weekday: ").lower()
+
     connection = sqlite3.connect("railwaySystem.db")
     cursor = connection.cursor()
-    
-
+    cursor.execute('''
+        SELECT TrainRoute.trainRouteId
+        FROM StationOnTrack
+        INNER JOIN TimeTableEntry ON
+            TimeTableEntry.trackSectionId = StationOnTrack.trackSectionId AND
+            TimeTableEntry.stationIndex = StationOnTrack.stationIndex
+        INNER JOIN TrainRoute ON
+            TimeTableEntry.trainRouteId = TrainRoute.TrainRouteId
+        INNER JOIN RunsOnWeekday ON
+            TrainRoute.trainRouteId = RunsOnWeekday.trainRouteId
+        WHERE
+            StationOnTrack.stationName = ? AND
+            RunsOnWeekday.weekdayName = ?
+    ''', (station, weekday))
+    print(cursor.fetchall())
+    connection.commit()
     connection.close();
-
-def get_train_routes(station_name: str, weekday: str, cursor: Cursor) -> list:
-    # list is the return type of cursor.fetchall()
-    return []
 
 if __name__ == "__main__": main()
