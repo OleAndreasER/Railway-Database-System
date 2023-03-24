ffa87bb7f231b9d925979c2ad34f37630a8ab386
Andreas Li <andrea10@stud.ntnu.no>
2023-03-24 12:01:04 +0100

    story f done

--- story_f.sql
+++ story_f.sql
@@ -1,5 +1,15 @@
 /*
-f) Necessary data should be entered for the system to handle purchasing of tickets for the three
-train routes on Nordlandsbanen Monday April 3rd and Tuesday April 4th of this year. This can be
-done with a script, you do not need to program support for this functionality.
+f) Det skal legges inn nødvendige data slik at systemet kan håndtere billettkjøp for de tre togrutene
+på Nordlandsbanen, mandag 3. april og tirsdag 4. april i år. Dette kan gjøres med et skript, dere
+trenger ikke å programmere støtte for denne funksjonaliteten..
 */
+
+INSERT INTO TrainOccurence(trainOccurenceId, date, trainRouteId)
+VALUES
+    (1, "2023-04-03", 1),
+    (2, "2023-04-03", 2),
+    (3, "2023-04-03", 3),
+
+    (4, "2023-04-04", 1),
+    (5, "2023-04-04", 2),
+    (6, "2023-04-04", 3);
