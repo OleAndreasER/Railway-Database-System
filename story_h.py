667b7ef9e0b8e1c5fd08cc01fd4394bb01063954
oarorvik <oarorvik@stud.ntnu.no>
2023-03-26 20:34:15 +0200

    Pretty print h

--- story_h.py
+++ story_h.py
@@ -7,9 +7,9 @@ functionality should be programmed.
 '''
 
 def main():
-    customer_id = input("CustomerId: ")
-    date = input("Date (YYYY-MM-DD): ")
-    try: datetime.date.fromisoformat(date)
+    customer_id = input("Customer id: ")
+    today_date = input("Today's date (YYYY-MM-DD): ")
+    try: datetime.date.fromisoformat(today_date)
     except:
         print("Invalid date")
         return
@@ -19,12 +19,15 @@ def main():
 
     cursor.execute('''
         SELECT
+            CustomerOrder.orderNr,
             TrainOccurence.date,
             TimeTableEntry.time,
             StartStation.stationName,
             EndStation.stationName,
+            ChairCarTicket.carNr,
             ChairCarTicket.rowNr,
             ChairCarTicket.seatNr,
+            SleepingCarTicket.carNr,
             SleepingCarTicket.bedNr
         FROM
             CustomerOrder
@@ -48,11 +51,56 @@ def main():
             Customer.customerId = ? AND
             TrainOccurence.date >= ? AND
             TimeTableEntry.stationIndex = Ticket.startIndex
-    ''', (customer_id, date))
-    
+    ''', (customer_id, today_date))
 
-    for (date,tid,startStation,endStation,rowNr, seatNr, bedNr) in cursor.fetchall():
-        print(f"dato: {date} boardingtidspunkt: {tid} fra {startStation} til {endStation}. rad: {rowNr} sete: {seatNr} seng: {bedNr}")
+    # Group orders for printing
+    tickets_grouped = {}
+    for (
+        order_nr,
+        date,
+        time,
+        start_station,
+        end_station,
+        chair_car_nr,
+        row_nr,
+        seat_nr,
+        sleeping_car_nr,
+        bed_nr
+    ) in cursor.fetchall():
+        orders = tickets_grouped.setdefault(date, {})
+        order = orders.setdefault(order_nr, {
+            "time": time,
+            "start_station": start_station,
+            "end_station": end_station,
+            "chair_tickets": [],
+            "bed_tickets": []
+        })
+        if not seat_nr is None:
+            order["chair_tickets"].append((
+                chair_car_nr,
+                row_nr,
+                seat_nr
+            ))
+        elif not bed_nr is None:
+            order["bed_tickets"].append((
+                sleeping_car_nr,
+                bed_nr
+            ))
+
+    # Printing
+    for date, orders in tickets_grouped.items():
+        print(f"{date}:")
+        for order in orders.values():
+            print(f"{order['time'][:-3]} from {order['start_station']} to {order['end_station']}")
+            if order["chair_tickets"]:
+                print("Car - Row - Seat")
+                for car_nr, row_nr, seat_nr in order["chair_tickets"]:
+                    print(f"{car_nr} - {row_nr} - {seat_nr}")
+            if order["bed_tickets"]:
+                print("Car - Bed")
+                for (car_nr, bed_nr) in order["bed_tickets"]:
+                    print(f"{car_nr} - {bed_nr}")
+            print()
 
     connection.close();
 
