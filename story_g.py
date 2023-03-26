30f18c841e6daf1ca5cb58a7a8b0b1d014b87044
oarorvik <oarorvik@stud.ntnu.no>
2023-03-26 13:51:14 +0200

    Works mostly

--- story_g.py
+++ story_g.py
@@ -9,19 +9,22 @@ purchase the tickets they would like. This functionality should be programmed.
 '''
 
 def main():
-    '''
+    # DETERMINE TRAIN ROUTE
     date = input("Date (YYYY-MM-DD): ")
+    try: datetime.date.fromisoformat(date)
+    except:
+        print("Invalid date")
+        return
 
     time = input("Time (HH:MM): ")
     if not time_is_valid(time):
         print("Invalid time")
         return
     time += ":00"
-    '''
-    start_station = "Steinkjer"
-    end_station = "Mo i Rana"
-    date = "2023-04-03"
-    time = "00:57:00"
+
+    start_station = input("Start station: ")
+    end_station = input("End station: ")
+
 
     connection = sqlite3.connect("railwaySystem.db")
     cursor = connection.cursor()
@@ -38,65 +41,122 @@ def main():
         print("No such train route.")
         return
 
+    (
+        start_index,
+        end_index,
+        train_occurence_id,
+        arrangement_id,
+        track_section_id
+    ) = train_route_data
+
+    # AVAILABLE TICKETS
     available_chair_car_tickets = get_available_chair_car_tickets(
-        *train_route_data,
+        start_index,
+        end_index,
+        train_occurence_id,
+        arrangement_id,
         cursor
     )
-    for row in available_chair_car_tickets:
-        print(row)
 
-    (_, _, train_occurence_id, arrangement_id) = train_route_data
     available_sleeping_car_tickets = get_available_sleeping_car_tickets(
         train_occurence_id,
         arrangement_id,
         cursor
     )
-    for row in available_sleeping_car_tickets:
-        print(row)
+    pretty_print(available_chair_car_tickets, available_sleeping_car_tickets)
 
-    return
-    #TICKET PURCHASE EXAMPLE
-    customer_id = 1
-    train_occurence_id = 2
+    # DETERMINE CUSTOMER
+    customer_id = maybe_int_input("Enter your customer id: ")
+    if customer_id is None:
+        print("Invalid id")
+        return
 
+    customer = get_customer(customer_id, cursor)
+    if not customer:
+        print("No customer with this id")
+        return
+    
+    print(f"Hello {customer[1]}!")
+
+    # START CUSTOMER ORDER
     cursor.execute('''
         INSERT INTO CustomerOrder(day, time, customerId, trainOccurenceId)
         VALUES (DATE('now'), TIME(), ?, ?)
     ''', (customer_id, train_occurence_id))
-
     order_nr = cursor.lastrowid
-    track_section_id = 1
-    start_index = 0
-    end_index = 3
-    chair_car_id = 1
-    sleeping_car_id = 2
-    bed_nr = 4
-    row_nr = 2
-    seat_nr = 4
-    '''
-    insert_sleeping_car_ticket(
-        order_nr,
-        track_section_id,
-        start_index,
-        end_index,
-        sleeping_car_id,
-        bed_nr,
-        cursor
-    )
-    '''
-    insert_chair_car_ticket(
-        order_nr,
-        track_section_id,
-        start_index,
-        end_index,
-        chair_car_id,
-        seat_nr,
-        row_nr,
-        cursor
+
+    is_chair_car_id = lambda car_id: any(
+        car_id == chair_car[0]
+        for chair_car in available_chair_car_tickets
     )
 
-    connection.commit()
+    is_sleeping_car_id = lambda car_id: any(
+        car_id == sleeping_car[0]
+        for sleeping_car in available_sleeping_car_tickets
+    )
+
+    def purchase_chair_car_ticket(car_id: int, row_nr: int, seat_nr: int):
+        insert_chair_car_ticket(
+            order_nr,
+            track_section_id,
+            start_index,
+            end_index,
+            car_id,
+            seat_nr,
+            row_nr,
+            cursor
+        )
+        available_chair_car_tickets.remove((car_id, row_nr, seat_nr))
+    
+    def purchase_sleeping_car_ticket(car_id: int, bed_nr: int):
+        insert_sleeping_car_ticket(
+            order_nr,
+            track_section_id,
+            start_index,
+            end_index,
+            car_id,
+            bed_nr,
+            cursor
+        )
+        available_sleeping_car_tickets.remove((car_id, bed_nr))
+
+
+    # ADD TICKETS TO ORDER
+    purchased_ticket = False
+    while True:
+        if "y" != input("Do you want to add a ticket to your order (y/N)? ").lower():
+            break
+
+        car_id = maybe_int_input("Car: ")
+
+        if is_chair_car_id(car_id):
+            row_nr = maybe_int_input("Row: ")
+            seat_nr = maybe_int_input("Seat: ")
+            if (car_id, row_nr, seat_nr) in available_chair_car_tickets:
+                purchase_chair_car_ticket(car_id, row_nr, seat_nr)
+                purchased_ticket = True
+            else:
+                print("That is not an available seat")
+        elif is_sleeping_car_id(car_id):
+            bed_nr = maybe_int_input("Bed: ")
+            if (car_id, bed_nr) in available_sleeping_car_tickets:
+                purchase_sleeping_car_ticket(car_id, bed_nr)
+                purchased_ticket = True
+            else:
+                print("That is not an available bed")
+        else:
+            print("No such car")       
+    
+    # Only insert order if a ticket was purchased.
+    if purchased_ticket:
+        connection.commit()
+        print("Tickets purchased")
+
     connection.close()
+
+def maybe_int_input(prompt: str) -> int or None: 
+    try: return int(input(prompt))
+    except: return None
 
 def insert_ticket(
     order_nr: int,
@@ -157,7 +217,7 @@ def get_available_chair_car_tickets(
 ) -> list:
     cursor.execute('''
         SELECT
-            CarInArrangement.carNr,
+            CarInArrangement.carId,
             Seatrow.rowNr,
             Seat.seatNr
         FROM CarArrangement
@@ -186,10 +246,14 @@ def get_available_chair_car_tickets(
                     Ticket.ticketId = ChairCarTicket.ticketId
                 WHERE
                     TrainOccurence.trainOccurenceId = :train_occurence_id AND
-                    ((:start_index > Ticket.startIndex AND
+                    ((:start_index >= Ticket.startIndex AND
                     :start_index < Ticket.endIndex) OR
                     (:end_index > Ticket.startIndex AND
-                    :end_index < Ticket.endIndex))
+                    :end_index <= Ticket.endIndex) OR
+                    (Ticket.startIndex >= :start_index AND
+                    Ticket.startIndex < :end_index) OR
+                    (Ticket.endIndex > :start_index AND
+                    Ticket.endIndex <= :end_index))
             )
     ''', {
         "arrangement_id": arrangement_id,
@@ -244,14 +308,14 @@ def get_available_sleeping_car_tickets(
     return cursor.fetchall()
 
 def get_customer(
-    customer_nr: int,
+    customer_id: int,
     cursor: Cursor
 ) -> tuple[int, str] or None:
     cursor.execute('''
-        SELECT customerNr, name
+        SELECT customerId, name
         FROM Customer
         WHERE Customer.customerId = ?
-    ''', (customer_nr,))
+    ''', (customer_id,))
     return cursor.fetchone()
 
 # Assumes there is only one train occurence given this info.
@@ -268,7 +332,8 @@ def get_train_route_data(
             StartStation.stationIndex,
             EndStation.stationIndex,
             trainOccurenceId,
-            arrangementId
+            arrangementId,
+            TrainRoute.trackSectionId
         FROM TrainOccurence
         INNER JOIN TrainRoute ON
             TrainRoute.trainRouteId = TrainOccurence.trainRouteId
@@ -289,5 +354,17 @@ def get_train_route_data(
             EndStation.stationName = ?
     ''', (date, time, start_station, end_station))
     return cursor.fetchone()
+
+def pretty_print(seat_tickets: list, bed_tickets: list):
+    print("Chair cars:")
+    print("Car - Row - Seat")
+    for car_id, row_nr, seat_nr in seat_tickets:
+        print(f"{car_id} - {row_nr} - {seat_nr}")
+
+    print("Sleeping cars:")
+    print("Car - Compartment - Bed")
+    for car_id, bed_nr in bed_tickets:
+        print(f"{car_id} - {(bed_nr + 1) // 2} - {bed_nr}")
+
 
 if __name__ == "__main__": main()
