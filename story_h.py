8015f4a4843364084bd524ae910f53ed2757acd7
Andreas Li <andrea10@stud.ntnu.no>
2023-03-26 19:27:52 +0200

    story h does not have car Arrangement, print is also ugly

--- story_h.py
+++ story_h.py
@@ -1,13 +1,54 @@
 import sqlite3
+import datetime
+
 '''
 h) All information about purchases made for future trips should be available for a user. This
 functionality should be programmed.
 '''
 
 def main():
+    customer_id = input("CustomerId: ")
+    date = input("Date (YYYY-MM-DD): ")
+    try: datetime.date.fromisoformat(date)
+    except:
+        print("Invalid date")
+        return
+
     connection = sqlite3.connect("railwaySystem.db")
     cursor = connection.cursor()
+
+    cursor.execute('''
+        SELECT
+            TrainOccurence.date,
+            TimeTableEntry.time,
+            Ticket.StartIndex,
+            Ticket.endIndex,
+            ChairCarTicket.rowNr,
+            ChairCarTicket.seatNr,
+            SleepingCarTicket.bedNr
+        FROM
+            CustomerOrder
+        INNER JOIN Customer ON
+            CustomerOrder.customerId = Customer.customerId
+        INNER JOIN Ticket ON
+            CustomerOrder.orderNr = Ticket.orderNr
+        INNER JOIN TrainOccurence ON
+            TrainOccurence.trainOccurenceId = CustomerOrder.trainOccurenceId
+        INNER JOIN TimeTableEntry ON
+            TimeTableEntry.trainRouteId = TrainOccurence.trainRouteId
+        LEFT JOIN ChairCarTicket ON
+		    ChairCarTicket.ticketId = Ticket.ticketId
+	    LEFT JOIN SleepingCarTicket ON
+		    SleepingCarTicket.ticketId = Ticket.ticketId
+        WHERE 
+            Customer.customerId = ? AND
+            TrainOccurence.date >= ? AND
+            TimeTableEntry.stationIndex = Ticket.startIndex
+    ''', (customer_id, date))
     
+
+    for (date,tid,startStation,endStation,rowNr, seatNr, bedNr) in cursor.fetchall():
+        print(f"dato: {date} boardingtidspunkt: {tid} fra stasjon {startStation} til stasjon {endStation}. rad: {rowNr} sete: {seatNr} seng: {bedNr}")
 
     connection.close();
 
